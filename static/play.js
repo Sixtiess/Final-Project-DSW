34c11a85c0a214d9547794ea302a0ea391e69bde
danyewest97 <dane20052008@gmail.com>
2025-01-15 13:59:25 -0800

    Still need to update player hand client side with ajax

--- static/play.js
+++ static/play.js
@@ -10,13 +10,14 @@ $(document).ready(function() {
 		// });
 	// }
 	$(".actionButton").click(function(){
-		console.log($(this).value)
+		// console.log($(this).value)
 		$.ajax({
 			type: "POST",
 			url:"/action",
 			data: { "action" : $(this).val()}
-		})
+		});
 	
-		
+		player_hand = $(".player-hand");
+		player_hand.ajax.reload();
 	});
 });
